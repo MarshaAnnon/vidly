65a6787e7a940f79373d08893b81ffefca1bf0e1
marsha <marshaannon@gmail.com>
2020-12-27 21:40:09 -0500

    add route to delete genre

--- index.js
+++ index.js
@@ -60,6 +60,14 @@ app.put('/api/genres/:id', (req, res) => {
   res.send(genre);
 });
 
+app.delete('/api/genres/:id', (req, res) => {
+  const genre = genres.find(genre => genre.id === parseInt(req.params.id));
+  if (!genre) return res.status(404).send('Sorry, the genre with that given ID was not found');
+
+  const index = genres.indexOf(genre);
+  genres.splice(index, 1);
+  res.send(genre);
+});
 
 
 function validateGenre(genre) {
