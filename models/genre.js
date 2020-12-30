2b726ebbaea05f548926e66cf3fdae5c761640b0
Marsha Annon”git config --global user.name Marsha <marshaannon@gmail.com>
2020-12-29 21:24:48 -0500

    modify mongoose set up create a genreSchema object and mongoose
    so they may be referenced in a movie file

--- models/genre.js
+++ models/genre.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose');
 const Joi = require('joi');
 
-const Genre = mongoose.model('Genre', new mongoose.Schema({
+const genreSchema = new mongoose.Schema({
     name: {
       type: String,
       required: true,
       minlength: 5,
       maxlength: 50
     }
-  }));
+  });
+
+  const Genre = mongoose.model('Genre', genreSchema);
 
   function validateGenre(genre) {
     const schema = Joi.object({
@@ -19,5 +21,6 @@ const Genre = mongoose.model('Genre', new mongoose.Schema({
     return schema.validate(genre);
   }
 
+exports.genreSchema = genreSchema;
 exports.Genre = Genre;
 exports.validate = validateGenre;
