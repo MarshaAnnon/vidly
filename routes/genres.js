796997abe6329fde81495511416681cc169dc7d5
Marsha Annon”git config --global user.name Marsha <marshaannon@gmail.com>
2020-12-30 15:27:49 -0500

    refactor post route change let to const and await movie.save() no longer reassigning the variable

--- routes/genres.js
+++ routes/genres.js
@@ -1,7 +1,6 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { Genre, validate } = require('../models/genre')
-
+const { Genre, validate } = require("../models/genre");
 
 // const genres = [
 //   { id: 1, name: "Action" },
@@ -21,44 +20,57 @@ const { Genre, validate } = require('../models/genre')
 // ]
 
 //gets all of the genre objects
-router.get('/', async (req,res) => {
-  const genres = await Genre.find().sort('name');
-  res.send(genres)
+router.get("/", async (req, res) => {
+  const genres = await Genre.find().sort("name");
+  res.send(genres);
 });
 
-router.get('/:id', async (req, res) => {
-  const genre = await Genre.findById(req.params.id)
+router.get("/:id", async (req, res) => {
+  const genre = await Genre.findById(req.params.id);
 
-  if (!genre) return res.status(404).send('Sorry, the genre with that given ID was not found');
+  if (!genre)
+    return res
+      .status(404)
+      .send("Sorry, the genre with that given ID was not found");
   res.send(genre);
 });
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  let genre = new Genre({ name: req.body.name });
-  genre = await genre.save();
+  const genre = new Genre({ name: req.body.name });
+  await genre.save();
   res.send(genre);
 });
 
-router.put('/:id', async (req, res) => {
-  const { error } = validate(req.body)
+router.put("/:id", async (req, res) => {
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  const genre =  await Genre.findByIdAndUpdate(req.params.id, { name: req.body.name}, {
-    new: true
-  })
+  const genre = await Genre.findByIdAndUpdate(
+    req.params.id,
+    { name: req.body.name },
+    {
+      new: true,
+    }
+  );
 
-  if (!genre) return res.status(404).send('Sorry, the genre with that given ID was not found');
-  
+  if (!genre)
+    return res
+      .status(404)
+      .send("Sorry, the genre with that given ID was not found");
+
   res.send(genre);
 });
 
-router.delete('/:id', async (req, res) => {
-  const genre =  await Genre.findByIdAndRemove(req.params.id);
-  
-  if (!genre) return res.status(404).send('Sorry, the genre with that given ID was not found');
+router.delete("/:id", async (req, res) => {
+  const genre = await Genre.findByIdAndRemove(req.params.id);
+
+  if (!genre)
+    return res
+      .status(404)
+      .send("Sorry, the genre with that given ID was not found");
 
   res.send(genre);
 });
