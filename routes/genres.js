9c2dbefec6eba3defd1f762ed26778392bbbe069
Marsha Annon”git config --global user.name Marsha <marshaannon@gmail.com>
2020-12-30 15:28:41 -0500

    refactor post route change let to const and await movie.save() no longer reassigning the variable

--- routes/genres.js
+++ routes/genres.js
@@ -1,6 +1,6 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const { Genre, validate } = require("../models/genre");
+const { Genre, validate } = require('../models/genre');
 
 // const genres = [
 //   { id: 1, name: "Action" },
@@ -20,22 +20,22 @@ const { Genre, validate } = require("../models/genre");
 // ]
 
 //gets all of the genre objects
-router.get("/", async (req, res) => {
-  const genres = await Genre.find().sort("name");
+router.get('/', async (req, res) => {
+  const genres = await Genre.find().sort('name');
   res.send(genres);
 });
 
-router.get("/:id", async (req, res) => {
+router.get('/:id', async (req, res) => {
   const genre = await Genre.findById(req.params.id);
 
   if (!genre)
     return res
       .status(404)
-      .send("Sorry, the genre with that given ID was not found");
+      .send('Sorry, the genre with that given ID was not found');
   res.send(genre);
 });
 
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -44,7 +44,7 @@ router.post("/", async (req, res) => {
   res.send(genre);
 });
 
-router.put("/:id", async (req, res) => {
+router.put('/:id', async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -59,18 +59,18 @@ router.put("/:id", async (req, res) => {
   if (!genre)
     return res
       .status(404)
-      .send("Sorry, the genre with that given ID was not found");
+      .send('Sorry, the genre with that given ID was not found');
 
   res.send(genre);
 });
 
-router.delete("/:id", async (req, res) => {
+router.delete('/:id', async (req, res) => {
   const genre = await Genre.findByIdAndRemove(req.params.id);
 
   if (!genre)
     return res
       .status(404)
-      .send("Sorry, the genre with that given ID was not found");
+      .send('Sorry, the genre with that given ID was not found');
 
   res.send(genre);
 });
