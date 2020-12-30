47aca44ea4828932b1b0d19b2324a8e85b9f6fc8
Marsha Annon”git config --global user.name Marsha <marshaannon@gmail.com>
2020-12-30 15:31:28 -0500

    apply prettier

--- routes/rentals.js
+++ routes/rentals.js
@@ -1,22 +1,25 @@
 const express = require('express');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const Fawn = require('fawn');
 const router = express.Router();
-const { Customer } = require('./models/customer');
+const { Customer } = require('../models/customer');
 const { Movie } = require('../models/movie');
 const { Rental, validate } = require('../models/rental');
 
 Fawn.init(mongoose);
 
-router.get('/', async (req,res) => {
+router.get('/', async (req, res) => {
   const rentals = await Rental.find().sort('-dateOut');
-  res.send(rentals)
+  res.send(rentals);
 });
 
 router.get('/:id', async (req, res) => {
-  const rental = await Rental.findById(req.params.id)
+  const rental = await Rental.findById(req.params.id);
 
-  if (!rental) return res.status(404).send('Sorry, the genre with that given ID was not found');
+  if (!rental)
+    return res
+      .status(404)
+      .send('Sorry, the genre with that given ID was not found');
   res.send(rental);
 });
 
@@ -30,53 +33,67 @@ router.post('/', async (req, res) => {
   const movie = await Movie.findById(req.body.movieID);
   if (!movie) return res.status(400).send('Invalid movie.');
 
-  if (movie.numberInStock === 0) return res.status(400).send('Movie is not in stock.');
+  if (movie.numberInStock === 0)
+    return res.status(400).send('Movie is not in stock.');
 
-  let rental = new Rental({ 
-        customer: {
-            _id: customer._id,
-            name: customer.name,
-            phone: customer.phone
-        },
-        movie: {
-            _id: movie._id,
-            title: movie.title,
-            dailyRentalRate: movie.dailyRentalRate
+  let rental = new Rental({
+    customer: {
+      _id: customer._id,
+      name: customer.name,
+      phone: customer.phone,
+    },
+    movie: {
+      _id: movie._id,
+      title: movie.title,
+      dailyRentalRate: movie.dailyRentalRate,
+    },
+  });
+
+  try {
+    new Fawn.Task()
+      .save('rentals', rental)
+      .update(
+        'movies',
+        { _id: movies._id },
+        {
+          $inc: { numberInStock: -1 },
         }
-    });
+      )
+      .run();
 
-    try {
-        new Fawn.Task()
-            .save('rentals', rental)
-            .update('movies', { _id: movies._id }, {
-                $inc: { numberInStock: -1 }
-            })
-            .run();
-    
-        res.send(rental);
-    }
-    catch(ex) {
-        res.status(500).send('Something went wrong.');
-    }
+    res.send(rental);
+  } catch (ex) {
+    res.status(500).send('Something went wrong.');
+  }
 });
 
 router.put('/:id', async (req, res) => {
-  const { error } = validate(req.body)
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  const movie =  await Rental.findByIdAndUpdate(req.params.id, { name: req.body.name}, {
-    new: true
-  })
+  const movie = await Rental.findByIdAndUpdate(
+    req.params.id,
+    { name: req.body.name },
+    {
+      new: true,
+    }
+  );
 
-  if (!movie) return res.status(404).send('Sorry, the genre with that given ID was not found');
-  
+  if (!movie)
+    return res
+      .status(404)
+      .send('Sorry, the genre with that given ID was not found');
+
   res.send(movie);
 });
 
 router.delete('/:id', async (req, res) => {
-  const movie =  await Rental.findByIdAndRemove(req.params.id);
-  
-  if (!movie) return res.status(404).send('Sorry, the genre with that given ID was not found');
+  const movie = await Rental.findByIdAndRemove(req.params.id);
+
+  if (!movie)
+    return res
+      .status(404)
+      .send('Sorry, the genre with that given ID was not found');
 
   res.send(movie);
 });
