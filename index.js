a6b96d36049d725a1b32743b2c92d4aa4b342d81
Marsha Annon”git config --global user.name Marsha <marshaannon@gmail.com>
2020-12-31 11:52:53 -0500

    add error handling to app.use & require error from middleware/error

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const express = require('express');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const mongoose = require('mongoose');
+const { use } = require('./routes/auth');
 const auth = require('./routes/auth');
 const customers = require('./routes/customers');
 const genres = require('./routes/genres');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
 const users = require('./routes/users');
+const error = require('./middleware/error');
 
 const app = express();
 
@@ -29,6 +31,7 @@ app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
 app.use('/api/auth', auth);
+app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
