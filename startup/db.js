86c5a5c32d5671a11cf2f19d25618aea5c156779
Marsha Annon”git config --global user.name Marsha <marshaannon@gmail.com>
2021-01-04 17:51:40 -0500

    extracted code from index.js to correct file for separation of concerns

---
+++ startup/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const winston = require('winston');
+
+module.exports = function () {
+  mongoose
+    .connect('mongodb://localhost/vidly', {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    })
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch((err) => console.error('Could not connect to MongoDB...'));
+};
