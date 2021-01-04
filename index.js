602658c9274826d86582635f0139de0961be993f
Marsha Annon”git config --global user.name Marsha <marshaannon@gmail.com>
2021-01-04 17:49:55 -0500

    fix order of requires

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+const express = require('express');
+const winston = require('winston');
+const app = express();
+
 require('express-async-errors');
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config')();
 require('./startup/validation')();
-const express = require('express');
-const winston = require('winston');
-const app = express();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
