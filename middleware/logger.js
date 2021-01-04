dbf8b5ef437e5143aa5cfc0782c9564fa40b7d16
Marsha Annon”git config --global user.name Marsha <marshaannon@gmail.com>
2021-01-04 17:25:18 -0500

    Add transports for Console and MongoDB to log errors to server

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,7 +1,21 @@
 const { createLogger, transports } = require('winston');
+require('winston-mongodb');
 
 module.exports = createLogger({
-  transports: new transports.File({
-    filename: 'logfile.log',
-  }),
+  transports: [
+    new transports.File({
+      filename: 'logfile.log',
+    }),
+    new transports.Console({ colorize: true, prettyPink: true }),
+    new transports.MongoDB({
+      level: 'error',
+      //mongo database connection link
+      db: 'mongodb://localhost/vidly',
+      options: {
+        useUnifiedTopology: true,
+      },
+      // A collection to save json formatted logs
+      collection: 'server_logs',
+    }),
+  ],
 });
