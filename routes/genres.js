2b12adabd0d47d71ecf1574aec1683c0398a6a3b
Marsha Annon”git config --global user.name Marsha <marshaannon@gmail.com>
2021-01-06 15:43:47 -0500

    add validateObjectId require and parameter in router.get('/:id'

--- routes/genres.js
+++ routes/genres.js
@@ -3,20 +3,23 @@ const router = express.Router();
 const { Genre, validate } = require('../models/genre');
 const admin = require('../middleware/admin');
 const auth = require('../middleware/auth');
+const validateObjectId = require('../middleware/validateObjectId');
+const mongoose = require('mongoose');
 
 router.get('/', async (req, res) => {
-  throw new Error('Could not get the genres.');
+  // throw new Error('Could not get the genres.');
   const genres = await Genre.find().sort('name');
   res.send(genres);
 });
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', validateObjectId, async (req, res) => {
   const genre = await Genre.findById(req.params.id);
 
   if (!genre)
     return res
       .status(404)
-      .send('Sorry, the genre with that given ID was not found');
+      .send('Sorry, the genre with that given ID was not found.');
+
   res.send(genre);
 });
 
